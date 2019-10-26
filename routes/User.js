18d801315f057cf3b71732cac0f5cd2cdbd30e32
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-26 12:31:58 -0700

    Created minimal authentication without tokens

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,7 @@
-const errrors = require('restify-errors');
+const errors = require('restify-errors');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const auth = require('../auth');
 
 module.exports = server => {
   //Register User
@@ -27,4 +28,19 @@ module.exports = server => {
       });
     });
   });
+
+  //Auth User
+  server.post('/auth', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      //Authenticate User
+      const user = await auth.authenticate(email, password);
+      res.send(200)
+      console.log(user)
+      next()
+    } catch (err) {
+      return next(new errors.UnauthorizedError(err));
+    }
+  })
 }
